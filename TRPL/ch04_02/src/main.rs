f963c3b65c85c846c404ad22da3ff51310bc1571
sunakan <sunakn03@gmail.com>
2021-04-10 16:43:55 +0900

    所有権はreturnするとちゃんとmoveしてくれる

--- TRPL/ch04_02/src/main.rs
+++ TRPL/ch04_02/src/main.rs
@@ -10,6 +10,11 @@ fn main() {
     makes_copy(x); // xも関数にムーブされるが、
                    // i32はCopyなので、この後にxを使っても
                    // 大丈夫
+
+    let s2 = String::from("hello2");
+    let s3 = takes_ownership_and_return(s2);
+    println!("{}", s3);
+
 } // ここでxがスコープを抜け、sもスコープを抜ける。ただし、sの値はムーブされているので、何も特別なことは起こらない。
   //
 
@@ -23,3 +28,8 @@ fn makes_copy(some_integer: i32) {
     // some_integerがスコープに入る
     println!("{}", some_integer);
 } // ここでsome_integerがスコープを抜ける。何も特別なことはない。
+
+fn takes_ownership_and_return(some_string: String) -> String {
+    println!("{}", some_string);
+    some_string
+}
