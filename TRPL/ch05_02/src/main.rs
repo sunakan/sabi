9c7644582cce183f7083aca6ae038e508c6122cb
sunakan <sunakn03@gmail.com>
2021-04-11 17:05:26 +0900

    struct初歩２

---
+++ TRPL/ch05_02/src/main.rs
@@ -0,0 +1,27 @@
+#[derive(Debug)]
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
+impl Rectangle {
+    fn area(&self) -> u32 { // これはメソッド
+        self.width * self.height
+    }
+    fn can_hold(&self, other: &Rectangle) -> bool { // メソッド
+        self.width > other.width && self.height > other.height
+    }
+    fn square(size: u32) -> Rectangle { // Not メソッド、Yes 関連関数(Associated Function)
+        Rectangle { width: size, height: size }
+    }
+}
+
+fn main() {
+    let rect1 = Rectangle { width: 30, height: 50 };
+    let rect2 = Rectangle { width: 10, height: 40 };
+    let rect3 = Rectangle { width: 60, height: 45 };
+
+    // rect1にrect2ははまり込む？
+    println!("Can rect1 hold rect2? {}", rect1.can_hold(&rect2));
+    println!("Can rect1 hold rect3? {}", rect1.can_hold(&rect3));
+}
