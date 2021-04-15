fb0c7ea55331bd0c0227ee51fbdffde97e5cfaa5
sunakan <sunakn03@gmail.com>
2021-04-16 08:07:01 +0900

    collection初歩: hashmap

---
+++ TRPL/ch08_02/src/main.rs
@@ -0,0 +1,17 @@
+fn main() {
+    use std::collections::HashMap;
+
+    let field_name = String::from("Favorite color");
+    let field_value = String::from("Blue");
+
+    let mut map = HashMap::new();
+    map.insert(field_name, field_value);
+    // field_name and field_value are invalid at this point, try using them and
+    // see what compiler error you get!
+    // field_nameとfield_valueはこの時点で無効になる。試しに使ってみて
+    // どんなコンパイルエラーが出るか確認してみて！
+    //println!("{}", field_name);
+
+    let v = map.get(&String::from("Blue"));
+    println!("{:?}",v);
+}
