3df49c44bf338ea2a0774fc5e3d4d7fc4d204351
sunakan <sunakn03@gmail.com>
2021-04-16 08:06:43 +0900

    collection 初歩

---
+++ TRPL/ch08_01/src/main.rs
@@ -0,0 +1,9 @@
+fn main() {
+    // let s1 = String::from("hello");
+    // let h = s1[0];
+    let len = String::from("Здравствуйте").len();
+    println!("{}", len);
+    let hello = String::from("Здравствуйте");
+    let s = &hello[0..4];
+    println!("{}",s);
+}
