b62ab02cd8a645d7c62760b3f218a8f98a488122
sunakan <sunakn03@gmail.com>
2021-04-29 14:50:02 +0900

    iterator初歩

---
+++ TRPL/ch13_04_iterator/src/lib.rs
@@ -0,0 +1,64 @@
+struct Counter {
+    count: u32,
+}
+
+impl Counter {
+    fn new() -> Counter {
+        Counter { count: 0 }
+    }
+}
+
+impl Iterator for Counter {
+    type Item = u32;
+    fn next(&mut self) -> Option<Self::Item> {
+        self.count += 1;
+        if self.count < 6 {
+            Some(self.count)
+        } else {
+            None
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn a() {
+        let mut counter = Counter::new();
+        assert_eq!(counter.next(), Some(1));
+        assert_eq!(counter.next(), Some(2));
+        assert_eq!(counter.next(), Some(3));
+        assert_eq!(counter.next(), Some(4));
+        assert_eq!(counter.next(), Some(5));
+        assert_eq!(counter.next(), None);
+        assert_eq!(counter.next(), None);
+        assert_eq!(counter.next(), None);
+    }
+
+    #[test]
+    fn b() {
+        let sum: u32 = Counter::new().zip(Counter::new().skip(1))
+            .map(|(a, b)| a * b)
+            .filter(|x| x % 3 == 0)
+            .sum();
+        assert_eq!(18, sum);
+    }
+
+    // zipは少ない方に合わせる
+    #[test]
+    fn c() {
+        let c: Vec<(u32, u32)> = Counter::new().zip(Counter::new().skip(1))
+            .collect();
+        assert_eq!(
+            vec![
+                (1, 2),
+                (2, 3),
+                (3, 4),
+                (4, 5),
+            ],
+            c
+        );
+    }
+}
